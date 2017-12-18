166e837b752fb5a06e7b67f9c8656c1ffe45a1ba
Bejoy Mathew <bejoygm@gmail.com>
2017-12-18 16:02:23 +0530

    initial commit

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles.sass';
+
+class Footer extends Component {
+  render() {
+    return (
+      <footer className="footer">
+        <p className="has-link" />
+      </footer>
+    );
+  }
+}
+
+export default Footer;
